93d416aae24c83068f95434868b642ad795fbb6e
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-25 13:28:22 -0300

    Initial commit

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/BossesOfMassDestruction.java
@@ -0,0 +1,19 @@
+package com.cerbon.bosses_of_mass_destruction;
+
+import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BMDConstants.MOD_ID)
+public class BossesOfMassDestruction {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BossesOfMassDestruction() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/util/BMDConstants.java
@@ -0,0 +1,5 @@
+package com.cerbon.bosses_of_mass_destruction.util;
+
+public class BMDConstants {
+    public static final String MOD_ID = "bosses_of_mass_destruction";
+}
