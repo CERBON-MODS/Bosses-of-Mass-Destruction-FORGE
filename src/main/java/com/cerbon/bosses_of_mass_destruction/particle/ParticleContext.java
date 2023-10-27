0a944c99353ead9c6519b9f00b0943a8fa99463a
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-26 21:25:57 -0300

    Minor change

--- src/main/java/com/cerbon/bosses_of_mass_destruction/particle/ParticleContext.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/particle/ParticleContext.java
@@ -5,5 +5,10 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.world.phys.Vec3;
 
-public record ParticleContext(SpriteSet spriteSet, ClientLevel level, Vec3 pos, Vec3 vel, Boolean cycleSprites) {
+public record ParticleContext(
+        SpriteSet spriteSet,
+        ClientLevel level,
+        Vec3 pos,
+        Vec3 vel,
+        Boolean cycleSprites) {
 }
