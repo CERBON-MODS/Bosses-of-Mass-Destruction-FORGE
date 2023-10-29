6fa760692b02bc62a9f9d295902a63dc6ae682d9
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-28 23:53:00 -0300

    Add BooleanFlag class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/api/maelstrom/general/data/BooleanFlag.java
@@ -0,0 +1,17 @@
+package com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.data;
+
+public class BooleanFlag {
+    private boolean flag = false;
+
+    public void flag() {
+        flag = true;
+    }
+
+    public boolean getAndReset() {
+        boolean toReturn = flag;
+        if (flag) {
+            flag = false;
+        }
+        return toReturn;
+    }
+}
