c5693461e523041f285291e62dd015e364c08b33
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-29 15:00:42 -0300

    Make cometTrail also return ClientParticleBuilder

--- src/main/java/com/cerbon/bosses_of_mass_destruction/particle/ParticleFactories.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/particle/ParticleFactories.java
@@ -4,8 +4,8 @@ import com.cerbon.bosses_of_mass_destruction.util.BMDColors;
 import com.cerbon.bosses_of_mass_destruction.api.maelstrom.static_utilities.MathUtils;
 
 public class ParticleFactories {
-    public static void cometTrail(){
-        new ClientParticleBuilder(BMDParticles.DISAPPEARING_SWIRL.get())
+    public static ClientParticleBuilder cometTrail(){
+        return new ClientParticleBuilder(BMDParticles.DISAPPEARING_SWIRL.get())
                 .color(f -> MathUtils.lerpVec(f, BMDColors.COMET_BLUE, BMDColors.FADED_COMET_BLUE))
                 .brightness(BMDParticles.FULL_BRIGHT)
                 .scale(f -> 0.5f + f * 0.3f);
