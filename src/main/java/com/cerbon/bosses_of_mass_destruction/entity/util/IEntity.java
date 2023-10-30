53d173bb666aa209b351532b23329e630e406dcf
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 16:03:13 -0300

    Change methods name

--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IEntity.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IEntity.java
@@ -3,11 +3,11 @@ package com.cerbon.bosses_of_mass_destruction.entity.util;
 import net.minecraft.world.phys.Vec3;
 
 public interface IEntity {
-    Vec3 getVel();
+    Vec3 getDeltaMovement();
     Vec3 getPos();
     Vec3 getEyePos();
-    Vec3 getRotationVector();
-    int getAge();
+    Vec3 getLookAngle();
+    int getTickCount();
     boolean isAlive();
     IEntity target();
 }
