77844378660950fea1bd007de1510bb89498f6b6
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 15:03:01 -0300

    Change method name

--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IMobEffectFilter.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IMobEffectFilter.java
@@ -3,5 +3,5 @@ package com.cerbon.bosses_of_mass_destruction.entity.util;
 import net.minecraft.world.effect.MobEffectInstance;
 
 public interface IMobEffectFilter {
-    boolean canHaveMobEffect(MobEffectInstance effect);
+    boolean canBeAffected(MobEffectInstance effect);
 }
