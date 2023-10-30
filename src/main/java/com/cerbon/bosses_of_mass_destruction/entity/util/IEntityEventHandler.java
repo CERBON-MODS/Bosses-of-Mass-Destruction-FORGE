b0ac5293f3c9fabf998d7e9069f0885dc69b8c02
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 15:22:40 -0300

    Change interface and method name

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IEntityEventHandler.java
@@ -0,0 +1,5 @@
+package com.cerbon.bosses_of_mass_destruction.entity.util;
+
+public interface IEntityEventHandler {
+    void handleEntityEvent(byte status);
+}
--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IStatusHandler.java
+++
@@ -1,5 +0,0 @@
-package com.cerbon.bosses_of_mass_destruction.entity.util;
-
-public interface IStatusHandler {
-    void handleClientStatus(byte status);
-}
