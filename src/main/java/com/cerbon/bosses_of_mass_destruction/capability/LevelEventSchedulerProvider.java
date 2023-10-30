6144b37cc03ee0779d0b1e2a763e09de63d6a928
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 12:17:56 -0300

    Add missing AutoRegisterCapability annotation

--- src/main/java/com/cerbon/bosses_of_mass_destruction/capability/LevelEventSchedulerProvider.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/capability/LevelEventSchedulerProvider.java
@@ -2,14 +2,12 @@ package com.cerbon.bosses_of_mass_destruction.capability;
 
 import com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.event.EventScheduler;
 import net.minecraft.core.Direction;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@AutoRegisterCapability
 public class LevelEventSchedulerProvider implements ICapabilityProvider {
     public static Capability<EventScheduler> EVENT_SCHEDULER = CapabilityManager.get(new CapabilityToken<>() {});
 
