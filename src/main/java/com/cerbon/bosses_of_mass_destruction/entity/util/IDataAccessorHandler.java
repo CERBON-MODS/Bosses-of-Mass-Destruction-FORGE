0d0f546562e4478bbeac5af2dd4b92dad3761243
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 14:40:14 -0300

    Add missing interface and change one interface name

--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IDataAccessorHandler.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/IDataAccessorHandler.java
@@ -2,6 +2,6 @@ package com.cerbon.bosses_of_mass_destruction.entity.util;
 
 import net.minecraft.network.syncher.EntityDataAccessor;
 
-public interface ITrackedDataHandler {
+public interface IDataAccessorHandler {
     void onTrackedDataSet(EntityDataAccessor<?> data);
 }
