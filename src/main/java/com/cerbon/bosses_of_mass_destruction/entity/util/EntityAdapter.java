f6f43f5d121a1dd2530eb896ff8d660fcc8dd7b6
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-30 16:03:29 -0300

    Add EntityAdapter class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/EntityAdapter.java
@@ -0,0 +1,54 @@
+package com.cerbon.bosses_of_mass_destruction.entity.util;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.phys.Vec3;
+
+public class EntityAdapter implements IEntity {
+    private final LivingEntity entity;
+
+    public EntityAdapter(LivingEntity entity) {
+        this.entity = entity;
+    }
+
+    @Override
+    public Vec3 getDeltaMovement() {
+        return entity.getDeltaMovement();
+    }
+
+    @Override
+    public Vec3 getPos() {
+        return entity.position();
+    }
+
+    @Override
+    public Vec3 getEyePos() {
+        return entity.getEyePosition();
+    }
+
+    @Override
+    public Vec3 getLookAngle() {
+        return entity.getLookAngle();
+    }
+
+    @Override
+    public int getTickCount() {
+        return entity.tickCount;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return entity.isAlive();
+    }
+
+    @Override
+    public IEntity target() {
+        if(entity instanceof Mob) {
+            LivingEntity target = ((Mob) entity).getTarget();
+            if(target != null)
+                return new EntityAdapter(target);
+        }
+        return null;
+    }
+}
+
