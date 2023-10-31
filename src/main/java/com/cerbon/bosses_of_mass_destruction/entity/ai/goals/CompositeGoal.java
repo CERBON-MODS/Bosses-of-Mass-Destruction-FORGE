d54d326048c43ee1d8e02f34c1f810bba29798ba
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-31 13:46:36 -0300

    Add CompositeGoal class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/ai/goals/CompositeGoal.java
@@ -0,0 +1,48 @@
+package com.cerbon.bosses_of_mass_destruction.entity.ai.goals;
+
+import net.minecraft.world.entity.ai.goal.Goal;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class CompositeGoal extends Goal {
+    private final List<Goal> goals;
+
+    public CompositeGoal(Goal... goals) {
+        this.goals = Arrays.asList(goals);
+        for (Goal goal : goals) {
+            this.getFlags().addAll(goal.getFlags());
+        }
+    }
+
+    @Override
+    public boolean canUse() {
+        return goals.stream().allMatch(Goal::canUse);
+    }
+
+    @Override
+    public boolean isInterruptable() {
+        return goals.stream().allMatch(Goal::isInterruptable);
+    }
+
+    @Override
+    public boolean requiresUpdateEveryTick() {
+        return true;
+    }
+
+    @Override
+    public void tick() {
+        goals.forEach(Goal::tick);
+    }
+
+    @Override
+    public void stop() {
+        goals.forEach(Goal::stop);
+    }
+
+    @Override
+    public void start() {
+        goals.forEach(Goal::start);
+    }
+}
+
