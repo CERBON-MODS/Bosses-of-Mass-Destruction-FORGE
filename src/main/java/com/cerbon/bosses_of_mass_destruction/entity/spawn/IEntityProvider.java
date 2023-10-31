a4f77867c8207687e8ad9fbd8897408d332be666
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-31 11:01:13 -0300

    Add all entity.spawn package interfaces

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/spawn/IEntityProvider.java
@@ -0,0 +1,7 @@
+package com.cerbon.bosses_of_mass_destruction.entity.spawn;
+
+import net.minecraft.world.entity.Entity;
+
+public interface IEntityProvider {
+    Entity getEntity();
+}
---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/spawn/ISpawnPosition.java
@@ -0,0 +1,7 @@
+package com.cerbon.bosses_of_mass_destruction.entity.spawn;
+
+import net.minecraft.world.phys.Vec3;
+
+public interface ISpawnPosition {
+    Vec3 getPos();
+}
