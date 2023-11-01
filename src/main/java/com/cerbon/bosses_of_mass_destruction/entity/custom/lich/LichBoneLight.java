f30f348e854561c7c2d4330c62f90226e0a6e95d
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-01 08:51:03 -0300

    Add LichBoneLight class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/custom/lich/LichBoneLight.java
@@ -0,0 +1,18 @@
+package com.cerbon.bosses_of_mass_destruction.entity.custom.lich;
+
+import com.cerbon.bosses_of_mass_destruction.client.render.IBoneLight;
+import software.bernie.geckolib.cache.object.GeoBone;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LichBoneLight implements IBoneLight {
+
+    @Override
+    public int getLightForBone(GeoBone bone, int packedLight) {
+        List<String> boneNames = Arrays.asList("crown_crystals", "crystal", "leftEye", "rightEye");
+        return boneNames.contains(bone.getName()) ? IBoneLight.fullbright : packedLight;
+    }
+}
+
+
