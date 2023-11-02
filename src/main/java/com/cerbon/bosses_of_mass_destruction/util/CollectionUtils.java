c971ee886c26d9b96e6d0a30daadc119621522ca
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-02 12:48:59 -0300

    Add useful methods to CollectionUtils class

--- src/main/java/com/cerbon/bosses_of_mass_destruction/util/CollectionUtils.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/util/CollectionUtils.java
@@ -2,10 +2,9 @@ package com.cerbon.bosses_of_mass_destruction.util;
 
 import com.mojang.datafixers.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class CollectionUtils {
     /**
@@ -52,5 +51,30 @@ public class CollectionUtils {
             current = next;
         }
         return result;
+    }
+
+    public static <T, R> List<R> map(Iterable<T> iterable, Function<T, R> transform){
+        return mapTo(iterable, new ArrayList<>(collectionSizeOrDefault(iterable, 10)), transform);
+    }
+
+    public static <T, R, C extends Collection<R>> C mapTo(Iterable<T> iterable, C destination, Function<T, R> transform){
+        for (T item : iterable)
+            destination.add(transform.apply(item));
+        return destination;
+    }
+
+    public static <T, R> List<R> mapIndexed(Iterable<T> iterable, BiFunction<Integer, T, R> transform){
+        return mapIndexedTo(iterable, new ArrayList<>(collectionSizeOrDefault(iterable, 10)), transform);
+    }
+
+    public static <T, R, C extends Collection<R>> C mapIndexedTo(Iterable<T> iterable, C destination, BiFunction<Integer, T, R> transform) {
+        int index = 0;
+        for (T item : iterable)
+            destination.add(transform.apply(index++, item));
+        return destination;
+    }
+
+    public static <T> int collectionSizeOrDefault(Iterable<T> iterable, int default1){
+        return iterable instanceof Collection<T> c ? c.size() : default1;
     }
 }
