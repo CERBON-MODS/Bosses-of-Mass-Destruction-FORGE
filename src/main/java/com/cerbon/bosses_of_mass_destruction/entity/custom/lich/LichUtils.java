aceda85c939f5cc2e5c7ebbccb186de3192ea73a
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-02 11:09:03 -0300

    Add LichUtils class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/custom/lich/LichUtils.java
@@ -0,0 +1,36 @@
+package com.cerbon.bosses_of_mass_destruction.entity.custom.lich;
+
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.static_utilities.MathUtils;
+import com.cerbon.bosses_of_mass_destruction.entity.util.IEntity;
+import com.cerbon.bosses_of_mass_destruction.entity.util.IEntityStats;
+import com.cerbon.bosses_of_mass_destruction.util.BMDColors;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class LichUtils {
+    public static final List<Float> hpPercentRageModes = Arrays.asList(0.0f, 0.25f, 0.5f, 0.75f, 1.0f);
+    public static final int textureSize = 256;
+    public static final Function<Float, Vec3> blueColorFade = f -> MathUtils.lerpVec(f, BMDColors.COMET_BLUE, BMDColors.FADED_COMET_BLUE);
+
+    public static long timeToNighttime(long currentTime) {
+        long dayLength = 24000L;
+        long midnight = 16000L;
+        return (currentTime - (currentTime % dayLength)) + midnight;
+    }
+
+    public static void cappedHeal(IEntity iEntity, IEntityStats stats, List<Float> hpPercentRageModes, float healingStrength, Function<Float, Void> heal) {
+        if (iEntity.isAlive()) {
+            float targetHealthRatio = MathUtils.roundedStep(stats.getHealth() / stats.getMaxHealth(), hpPercentRageModes, false);
+            float healAmt = Mth.clamp(targetHealthRatio * stats.getMaxHealth() - stats.getHealth() - 1, 0f, healingStrength);
+
+            if (healAmt > 0) {
+                heal.apply(healAmt);
+            }
+        }
+    }
+}
+
