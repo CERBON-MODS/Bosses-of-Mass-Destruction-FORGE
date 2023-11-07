2a484794c6ffbe7053ca78e14129df3aac321c60
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-06 21:23:44 -0300

    Add ObsidilithUtils class

---
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/custom/obsidilith/ObsidilithUtils.java
@@ -0,0 +1,112 @@
+package com.cerbon.bosses_of_mass_destruction.entity.custom.obsidilith;
+
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.event.EventScheduler;
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.event.TimedEvent;
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.static_utilities.MathUtils;
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.static_utilities.RandomUtils;
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.static_utilities.VecUtils;
+import com.cerbon.bosses_of_mass_destruction.capability.util.BMDCapabilities;
+import com.cerbon.bosses_of_mass_destruction.client.render.NodeBossBarRenderer;
+import com.cerbon.bosses_of_mass_destruction.entity.BMDEntities;
+import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
+import com.cerbon.bosses_of_mass_destruction.util.BMDUtils;
+import com.cerbon.bosses_of_mass_destruction.util.VanillaCopies;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+
+public class ObsidilithUtils {
+    private static final int textureSize = 256;
+    private static final ResourceLocation bossBarDividerTexture = new ResourceLocation(BMDConstants.MOD_ID, "textures/gui/obsidilith_boss_bar_dividers.png");
+    public static final List<Float> hpPillarShieldMilestones = List.of(0.0f, 0.25f, 0.5f, 0.75f, 1.0f);
+    public static final EntityDataAccessor<Boolean> isShielded = SynchedEntityData.defineId(ObsidilithEntity.class, EntityDataSerializers.BOOLEAN);
+    //TODO: Uncomment
+    //public static final NodeBossBarRenderer obsidilithBossBarRenderer = new NodeBossBarRenderer(BMDEntities.OBSIDILITH.get().getTranslationKey(), hpPillarShieldMilestones, bossBarDividerTexture, textureSize);
+    public static final List<Vec3> circlePos = MathUtils.buildBlockCircle(Math.pow(2.0, 2) + Math.pow(Math.pow(1.0, 2), 0.5));
+
+    public static final byte deathStatus = 3;
+    public static final byte burstAttackStatus = 5;
+    public static final byte waveAttackStatus = 6;
+    public static final byte spikeAttackStatus = 7;
+    public static final byte anvilAttackStatus = 8;
+    public static final byte pillarDefenseStatus = 9;
+    public static final int deathPillarHeight = 15;
+    public static final int ticksBetweenPillarLayer = 5;
+
+    public static Vec3 approximatePlayerNextPosition(List<Vec3> previousPosition, Vec3 currentPos) {
+        return previousPosition.stream()
+                .map(vec3 -> VecUtils.planeProject(vec3.subtract(currentPos), VecUtils.yAxis))
+                .reduce(Vec3.ZERO, Vec3::add)
+                .multiply(-0.5, -0.5, -0.5).add(currentPos);
+    }
+
+    public void onDeath(LivingEntity actor, int experienceDrop){
+        Level level = actor.level();
+
+        if (!level.isClientSide()){
+            BlockPos blockPos = actor.blockPosition();
+            Vec3 vecPos = actor.position();
+            EventScheduler eventScheduler = BMDCapabilities.getLevelEventScheduler(level);
+            level.explode(actor, actor.getX(), actor.getY(), actor.getZ(), 2.0f, Level.ExplosionInteraction.MOB);
+
+            for (int y = 0; y <= deathPillarHeight; y++){
+                int y1 = y;
+                eventScheduler.addEvent(
+                        new TimedEvent(
+                                () ->{
+                                    actor.playSound(SoundEvents.STONE_PLACE, 1.0f, BMDUtils.randomPitch(actor.getRandom()));
+
+                                    for (Vec3 pos : circlePos){
+                                        level.setBlockAndUpdate(
+                                                new BlockPos((int) pos.x, y1, (int) pos.z).offset(blockPos),
+                                                Blocks.OBSIDIAN.defaultBlockState()
+                                        );
+                                    }
+                                },
+                                y * ticksBetweenPillarLayer
+                        )
+                );
+            }
+
+            eventScheduler.addEvent(
+                    new TimedEvent(
+                            () -> {
+                                BlockPos chestPos = blockPos.above(deathPillarHeight + 1);
+                                level.setBlock(chestPos, Blocks.SHULKER_BOX.defaultBlockState(), 2);
+                                RandomizableContainerBlockEntity.setLootTable(level, actor.getRandom(), chestPos, new ResourceLocation(BMDConstants.MOD_ID, "chests/obsidilith"));
+                            },
+                            deathPillarHeight * ticksBetweenPillarLayer
+                    )
+            );
+
+            int expTicks = 20;
+            int expPerTick = (int) (experienceDrop / (float) expTicks);
+            Vec3 pillarTop = vecPos.add(VecUtils.yAxis.multiply(deathPillarHeight, deathPillarHeight, deathPillarHeight));
+            eventScheduler.addEvent(
+                    new TimedEvent(
+                            () -> {
+                                VanillaCopies.awardExperience(
+                                        expPerTick,
+                                        pillarTop.add(VecUtils.planeProject(RandomUtils.randVec(), VecUtils.yAxis).multiply(2.0, 2.0, 2.0)),
+                                        level
+                                );
+                            },
+                            deathPillarHeight * ticksBetweenPillarLayer,
+                            expTicks,
+                            () -> false
+                    )
+            );
+        }
+    }
+}
+
