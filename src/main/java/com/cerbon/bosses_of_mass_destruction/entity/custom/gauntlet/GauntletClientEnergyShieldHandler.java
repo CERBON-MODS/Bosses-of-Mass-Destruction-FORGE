b642c2ba4007f62e0465cd38697093acecfce367
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-16 09:32:46 -0300

    Fix gauntlet armor not stop rendering

--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/custom/gauntlet/GauntletClientEnergyShieldHandler.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/custom/gauntlet/GauntletClientEnergyShieldHandler.java
@@ -44,7 +44,7 @@ public class GauntletClientEnergyShieldHandler implements IDataAccessorHandler {
                             () -> !entity.isAlive() || !entity.getEntityData().get(GauntletEntity.isEnergized)
                     )
             );
-        }
+        }else energizedRenderAlpha = 0.0f;
     }
 
     private void spawnParticles(){
