a26b92a9a72a0939259db06d8fcbe49dda9a531f
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-28 13:36:16 -0300

    Change to animationPredicate

--- src/main/java/com/cerbon/bosses_of_mass_destruction/util/AnimationUtils.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/util/AnimationUtils.java
@@ -1,17 +1,17 @@
 package com.cerbon.bosses_of_mass_destruction.util;
 
+import com.cerbon.bosses_of_mass_destruction.entity.util.animation.AnimationPredicate;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.controller.AnimationController;
 
 public class AnimationUtils {
-    public static <T extends IAnimatable> AnimationController.IAnimationPredicate<T> createIdlePredicate(String animationName) {
-        return animationState -> {
+    public static <T extends IAnimatable> AnimationPredicate<T> createIdlePredicate(String animationName) {
+        return new AnimationPredicate<>(animationState -> {
             animationState.getController().setAnimation(
                     new AnimationBuilder().addAnimation(animationName, true)
             );
             return PlayState.CONTINUE;
-        };
+        });
     }
 }
