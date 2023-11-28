367ba63e32a1cf1ba8f2ad8b672d4c9f20862aec
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-11-28 12:24:57 -0300

    Add renderHelper to render method

--- src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/SimpleLivingGeoRenderer.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/entity/util/SimpleLivingGeoRenderer.java
@@ -90,6 +90,7 @@ public class SimpleLivingGeoRenderer<T extends LivingEntity & IAnimatable> exten
     @Override
     public void render(GeoModel model, T animatable, float partialTick, RenderType type, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         super.render(model, animatable, partialTick, type, poseStack, bufferSource, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        renderHelper.render(model, animatable, partialTick, type, poseStack, bufferSource, buffer, packedLight, packedOverlay, red, green, blue, alpha);
         int packetOverlay = overlayOverride != null ? overlayOverride.getOverlay() : packedOverlay;
         if (renderWithModel != null) renderWithModel.render(model, partialTick, poseStack, bufferSource, packedLight, packetOverlay, red, green, blue, alpha);
     }
