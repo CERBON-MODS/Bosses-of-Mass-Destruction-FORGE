b1008788e8ed4fbb8e553c2c8ade905ebaccb9e8
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-12-20 09:05:10 -0300

    Commit missing classes

--- src/main/java/com/cerbon/bosses_of_mass_destruction/BossesOfMassDestruction.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/BossesOfMassDestruction.java
@@ -1,5 +1,6 @@
 package com.cerbon.bosses_of_mass_destruction;
 
+import com.cerbon.bosses_of_mass_destruction.attachment.BMDAttachments;
 import com.cerbon.bosses_of_mass_destruction.block.BMDBlockEntities;
 import com.cerbon.bosses_of_mass_destruction.block.BMDBlocks;
 import com.cerbon.bosses_of_mass_destruction.config.BMDConfig;
@@ -8,27 +9,24 @@ import com.cerbon.bosses_of_mass_destruction.item.BMDCreativeModeTabs;
 import com.cerbon.bosses_of_mass_destruction.item.BMDItems;
 import com.cerbon.bosses_of_mass_destruction.particle.BMDParticles;
 import com.cerbon.bosses_of_mass_destruction.sound.BMDSounds;
-import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
 import com.cerbon.bosses_of_mass_destruction.structure.BMDStructures;
+import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
 import com.mojang.logging.LogUtils;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(BMDConstants.MOD_ID)
 public class BossesOfMassDestruction {
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public BossesOfMassDestruction() {
+    public BossesOfMassDestruction(IEventBus modEventBus) {
         AutoConfig.register(BMDConfig.class, JanksonConfigSerializer::new);
         AutoConfig.getConfigHolder(BMDConfig.class).getConfig().postInit();
         AutoConfig.getConfigHolder(BMDConfig.class).save();
-
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         BMDCreativeModeTabs.register(modEventBus);
         BMDBlocks.register(modEventBus);
@@ -36,11 +34,12 @@ public class BossesOfMassDestruction {
 
         BMDEntities.register(modEventBus);
         BMDBlockEntities.register(modEventBus);
+        BMDAttachments.register(modEventBus);
 
         BMDSounds.register(modEventBus);
         BMDParticles.register(modEventBus);
         BMDStructures.register(modEventBus);
 
-        MinecraftForge.EVENT_BUS.register(this);
+        //NeoForge.EVENT_BUS.register(this);
     }
 }
--- src/main/java/com/cerbon/bosses_of_mass_destruction/event/ForgeEvents.java
+++ src/main/java/com/cerbon/bosses_of_mass_destruction/event/ForgeEvents.java
@@ -1,70 +1,46 @@
 package com.cerbon.bosses_of_mass_destruction.event;
 
-import com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.event.EventScheduler;
+import com.cerbon.bosses_of_mass_destruction.api.maelstrom.general.data.HistoricalData;
+import com.cerbon.bosses_of_mass_destruction.attachment.BMDAttachments;
 import com.cerbon.bosses_of_mass_destruction.block.custom.LevitationBlockEntity;
-import com.cerbon.bosses_of_mass_destruction.capability.ChunkBlockCacheProvider;
-import com.cerbon.bosses_of_mass_destruction.capability.LevelEventSchedulerProvider;
-import com.cerbon.bosses_of_mass_destruction.capability.PlayerMoveHistoryProvider;
+import com.cerbon.bosses_of_mass_destruction.capability.util.BMDCapabilities;
 import com.cerbon.bosses_of_mass_destruction.entity.BMDEntities;
 import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.LogicalSide;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 
 @Mod.EventBusSubscriber(modid = BMDConstants.MOD_ID)
 public class ForgeEvents {
 
     @SubscribeEvent
-    public static void onAttachCapabilitiesLevel(AttachCapabilitiesEvent<Level> event){
-        if (event.getObject() != null) {
-            if (!event.getObject().getCapability(LevelEventSchedulerProvider.EVENT_SCHEDULER).isPresent())
-                event.addCapability(new ResourceLocation(BMDConstants.MOD_ID, "event_scheduler"), new LevelEventSchedulerProvider());
-
-            if (!event.getObject().getCapability(ChunkBlockCacheProvider.CHUNK_BLOCK_CACHE).isPresent())
-                event.addCapability(new ResourceLocation(BMDConstants.MOD_ID, "chunk_block_cache_capability"), new ChunkBlockCacheProvider());
-        }
-
-    }
-
-    @SubscribeEvent
-    public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
-        if(event.getObject() instanceof Player player)
-            if(!player.getCapability(PlayerMoveHistoryProvider.HISTORICAL_DATA).isPresent())
-                event.addCapability(new ResourceLocation(BMDConstants.MOD_ID, "player_move_history"), new PlayerMoveHistoryProvider());
-    }
-
-    @SubscribeEvent
     protected static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if(event.side == LogicalSide.SERVER) {
-            event.player.getCapability(PlayerMoveHistoryProvider.HISTORICAL_DATA).ifPresent(data -> {
-                Vec3 previousPosition = data.get(0);
-                Vec3 newPosition = event.player.position();
+        if(event.side == LogicalSide.CLIENT) return;
 
-                // Extremely fast movement in one tick is a sign of teleportation or dimension hopping, and thus we should clear history to avoid undefined behavior
-                if (previousPosition.distanceToSqr(newPosition) > 5)
-                    data.clear();
+        HistoricalData<Vec3> data = event.player.getData(BMDAttachments.HISTORICAL_DATA);
 
-                data.set(newPosition);
-            });
+        Vec3 previousPosition = data.get(0);
+        Vec3 newPosition = event.player.position();
 
-            LevitationBlockEntity.tickFlight((ServerPlayer) event.player);
-        }
+        // Extremely fast movement in one tick is a sign of teleportation or dimension hopping, and thus we should clear history to avoid undefined behavior
+        if (previousPosition.distanceToSqr(newPosition) > 5)
+            data.clear();
+
+        data.set(newPosition);
+
+        LevitationBlockEntity.tickFlight((ServerPlayer) event.player);
     }
 
     @SubscribeEvent
     public static void onLevelTick(TickEvent.LevelTickEvent event){
         if ((event.side == LogicalSide.SERVER || event.side == LogicalSide.CLIENT) && event.level.getGameTime() % 2 == 0)
-            event.level.getCapability(LevelEventSchedulerProvider.EVENT_SCHEDULER).ifPresent(EventScheduler::updateEvents);
+            BMDCapabilities.getLevelEventScheduler(event.level).updateEvents();
     }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
