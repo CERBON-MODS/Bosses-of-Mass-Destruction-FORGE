3fd99286485a4d6b46f158577501df0af1bb226b
Felipe <felipecerboncinix@gmail.com>
2024-01-29 16:02:11 -0300

    Rename event classes

--- Forge/src/main/java/com/cerbon/bosses_of_mass_destruction/forge/event/BMDEventsForge.java
+++ Forge/src/main/java/com/cerbon/bosses_of_mass_destruction/forge/event/BMDEventsForge.java
@@ -4,6 +4,6 @@ import com.cerbon.bosses_of_mass_destruction.util.BMDConstants;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = BMDConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class BossesOfMassDestructionEventsForge {
+public class BMDEventsForge {
 
 }
--- NeoForge/src/main/java/com/cerbon/bosses_of_mass_destruction/neoforge/event/BMDEventsNeo.java
+++ NeoForge/src/main/java/com/cerbon/bosses_of_mass_destruction/neoforge/event/BMDEventsNeo.java
@@ -1,5 +1,5 @@
 package com.cerbon.bosses_of_mass_destruction.neoforge.event;
 
 // Don't Forget to add Event Bus Subscriber | I can't add it without at least one method annotated with @SubscribeEvent
-public class BossesOfMassDestructionEventsNeo {
+public class BMDEventsNeo {
 }
